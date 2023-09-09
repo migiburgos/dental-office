11df8fd390e19641116254348909fe9ae3c1cc90
migiburgos <67326096+migiburgos@users.noreply.github.com>
2023-09-09 13:29:35 +0800

    Initialized Mongoose

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,8 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3001 || process.env.PORT;
-
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on Port ${PORT}...`);
-});
+module.exports = app;
