f8a1d8ab32eb6a9e9f4bfc2a1931c95f476601fb
migiburgos <67326096+migiburgos@users.noreply.github.com>
2023-09-11 02:30:03 +0800

    Removed unnecessary endpoint

--- backend/src/routes/appointments/appointments.router.js
+++ backend/src/routes/appointments/appointments.router.js
@@ -3,13 +3,11 @@ const {
   httpFetchAppointments,
   httpCreateAppointment,
   httpUpdateAppointments,
-  httpFetchAppointmentsByServiceAndDoctor,
 } = require("./appointments.controller");
 
 const appointmentsRouter = express.Router();
 
 appointmentsRouter.get("/", httpFetchAppointments);
-appointmentsRouter.get("/", httpFetchAppointmentsByServiceAndDoctor);
 appointmentsRouter.put("/:id", httpUpdateAppointments);
 appointmentsRouter.post("/create", httpCreateAppointment);
 
