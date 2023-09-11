6a4102c2fa0eb3527ed2d5c1c27080bca440d342
migiburgos <67326096+migiburgos@users.noreply.github.com>
2023-09-11 17:12:27 +0800

    Added Cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 
 const api = require("./routes/api");
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
