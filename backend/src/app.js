437c0e22d218324ab865a49f67e242f403ccb07d
migiburgos <67326096+migiburgos@users.noreply.github.com>
2023-09-12 22:12:41 +0800

    Deploying API only to Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
+// app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
